a90642fcd634dd6089b8c466039080594e7ece5c
Griko Nibras <griko@nibras.co>
2023-06-11 02:41:29 +0700

    feat: update tsup files constant

--- eslint/constants/index.js
+++ eslint/constants/index.js
@@ -4,5 +4,5 @@ module.exports = {
   JAVASCRIPT_FILES: ["*.js?(x)", "*.mjs", "*.cjs"],
   JEST_FILES: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
   TYPESCRIPT_FILES: ["*.d.ts", "*.ts?(x)", "*.mts", "*.cts"],
-  TSUP_FILES: ["tsup.config?(.*).{ts,js,cjs,json}"],
+  TSUP_FILES: ["tsup.config?(.*).{ts,mts,cts,js,mjs,cjs,json}"],
 };
